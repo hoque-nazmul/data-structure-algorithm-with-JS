69ad70de22d0ad0e0fb079a2e0e52915c5737ce4
Nazmul Hoque <coder.nazmul@gmail.com>
2020-05-10 21:49:24 +0600

    Defination added

--- Data Structure/LinkedList.js
+++ Data Structure/LinkedList.js
@@ -1,3 +1,10 @@
+// Linked List is one of the most interesting & important Data Structure. It is the list of elements called nodes that are connected together or linked together in a line. There are two types of linked list. For Example:
+
+// 1, Singly Linked List: Its node only points to the next Node.
+// 2. Doubly Linked List: Its node not only points to the next Node but also points to the previous Node.
+
+// Linked List consists of nodes, and each node has a value and a pointer to another node or null. Excepting the last Node, every node of Linked List points to the next node. The last node points to the null. It gives us a great privilege to do efficient insertion and removal of items without the need for reorganization. The beginning of the Linked List is known as Head and the end of the Linked List is known as Tail.
+
 class Node {
     constructor (data, next = null) {
         this.data = data;
@@ -124,13 +131,32 @@ class LinkedList {
         previous.next = node;
     }
 }
-
-
+// play with Linked List
 const myLinkedList = new LinkedList();
 
 myLinkedList.insertFirst(22);
 myLinkedList.insertFirst(11);
 
 console.log(myLinkedList);
+/*  expected output:
+    LinkedList {
+        head: Node { data: 11, next: Node { data: 22, next: null } }
+    }
+*/
+console.log(myLinkedList.size()); // expected output: 2
+
+console.log(myLinkedList.removeAt(1));
+console.log(myLinkedList);
+/*  expected output:
+    LinkedList { head: Node { data: 11, next: null } }
+*/
+
+console.log(myLinkedList.insertAt(1, 50));
+console.log(myLinkedList);
+/*  expected output:
+    LinkedList {
+        head: Node { data: 11, next: Node { data: 50, next: null } }
+    }
+*/
 
 
